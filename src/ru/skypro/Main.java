f8cadc0c390849b3c17bbdbda73876c1c12fe439
b.ulyashev <b.m.ulyashev@mail.ru>
2022-05-25 22:11:13 +0300

    исправление замечаний ДЗ

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -8,56 +8,75 @@ public class Main {
         task4();
     }
 
-    public static void task1() {
-        int[] a = {1, 2, 3};
+    private static void task1() {
+        int[] a = new int[]{1, 2, 3};
         double[] b = new double[] {1.57, 7.654, 9.986};
-        int[] c = new int[15];
+        int[] c = {23, 45, 87, 14, 94};
     }
 
-    public static void task2() {
-        int[] a = {1, 2, 3};
+    private static void task2() {
+        int[] a = new int[]{1, 2, 3};
         double[] b = new double[] {1.57, 7.654, 9.986};
-        int[] c = new int[] {23, 45, 87, 14, 94};
+        int[] c = {23, 45, 87, 14, 94};
 
-        for (int i = 0; i < a.length - 1; i++) {
-            System.out.print(a[i] + ", ");
+        for (int i = 0; i < a.length; i++) {
+            if(i == a.length - 1) {
+                System.out.println(a[i]);
+            } else {
+                System.out.print(a[i] + ", ");
+            }
         }
-        System.out.println(a[a.length - 1]);
 
-        for (int i = 0; i < b.length - 1; i++) {
-            System.out.print(b[i] + ", ");
+        for (int i = 0; i < b.length; i++) {
+            if(i == b.length - 1) {
+                System.out.println(b[i]);
+            } else {
+                System.out.print(b[i] + ", ");
+            }
         }
-        System.out.println(b[b.length - 1]);
 
-        for (int i = 0; i < c.length - 1; i++) {
-            System.out.print(c[i] + ", ");
+        for (int i = 0; i < c.length; i++) {
+            if(i == c.length - 1) {
+                System.out.println(c[i]);
+            } else {
+                System.out.print(c[i] + ", ");
+            }
         }
-        System.out.println(c[c.length - 1]);
     }
 
-    public static void task3() {
-        int[] a = {1, 2, 3};
+    private static void task3() {
+        int[] a = new int[]{1, 2, 3};
         double[] b = new double[] {1.57, 7.654, 9.986};
-        int[] c = new int[] {23, 45, 87, 14, 94};
+        int[] c = {23, 45, 87, 14, 94};
 
-        for (int i = a.length - 1; i > 0; i--) {
-            System.out.print(a[i] + ", ");
+        for (int i = a.length - 1; i >= 0; i--) {
+            if(i == 0) {
+                System.out.println(a[i]);
+            } else {
+                System.out.print(a[i] + ", ");
+            }
         }
-        System.out.println(a[0]);
 
-        for (int i = b.length - 1; i > 0; i--) {
-            System.out.print(b[i] + ", ");
-        }
-        System.out.println(b[0]);
 
-        for (int i = c.length - 1; i > 0; i--) {
-            System.out.print(c[i] + ", ");
+        for (int i = b.length - 1; i >= 0; i--) {
+            if(i == 0) {
+                System.out.println(b[i]);
+            } else {
+                System.out.print(b[i] + ", ");
+            }
         }
-        System.out.println(c[0]);
+
+        for (int i = c.length - 1; i >= 0; i--) {
+            if(i == 0) {
+                System.out.println(c[i]);
+            } else {
+                System.out.print(c[i] + ", ");
+            }
+        }
     }
 
-    public static void task4() {
-        int[] a = {1, 2, 3, 4, 5, 7, 9};
+    private static void task4() {
+        int[] a = new int[] {1, 2, 3};
         for (int i = 0; i < a.length; i++) {
             if (a[i] % 2 != 0) {
                 a[i]++;
